71cd11d75d112a61ed33b1e95a0f6bea5fb4d5a9
tsimandrey19777 <siblp@mail.ru>
2019-06-25 00:52:48 +0800

    Контролер главного шаблона mainapp, не хватает Моделей

--- food_delivery/mainapp/views.py
+++ food_delivery/mainapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+import random
 
-# Create your views here.
+
+def get_random_restoran():
+    restorans = Restorans.objects.all()
+
+    return random.sample(list(restorans), 1)[0]
+
+
+def get_hot_products():
+    restoran = get_random_restoran()
+    products = Products.object.filter(restoran=restoran, is_active=True)
+    hot_products = random.sample(list(products), 3)
+    return hot_products
+
+
+def main(request):
+    title = 'Главная'
+    hot_products = get_hot_products()
+
+    content = {'title': title,
+               'products': products,
+               }
+    return render(request, 'mainapp/index.html', content)
