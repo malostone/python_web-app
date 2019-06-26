13b8ff093d6d13c6cc5bee01e00e16f546fd4d20
tsimandrey19777 <siblp@mail.ru>
2019-06-26 16:17:45 +0800

    Revert "Отредактировал контролер главной страницы, прижал футер, создал шаблон главной страницы"
    This reverts commit de52d2bb7bd44456383c0edf367d8d54286ff71e.

--- food_delivery/food_delivery/settings.py
+++ food_delivery/food_delivery/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'mainapp.apps.MainappConfig',
     'mainapp',
 ]
 
@@ -119,11 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = {
+    os.path.join(BASE_DIR, 'static'),
+}
--- food_delivery/mainapp/views.py
+++ food_delivery/mainapp/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 import random
-from .models import Product, ProductCategory, ProductCompany
 
 
 def get_random_restoran():
     restorans = ProductCompany.objects.all()
-    # return restorans[0]
+
     return random.sample(list(restorans), 1)[0]
 
 
 def get_hot_products():
     restoran = get_random_restoran()
-    products = Product.objects.filter(company__name=restoran.name)
-    hot_products = random.sample(list(products), 3)[:2]
+    products = Product.object.filter(restoran=restoran, is_active=True)
+    hot_products = random.sample(list(products), 3)[:3]
     return hot_products
 
 
@@ -21,6 +20,6 @@ def main(request):
     hot_products = get_hot_products()
 
     content = {'title': title,
-               'products': hot_products,
+               'products': products,
                }
     return render(request, 'mainapp/index.html', content)
