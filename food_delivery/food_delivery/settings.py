de52d2bb7bd44456383c0edf367d8d54286ff71e
tsimandrey19777 <siblp@mail.ru>
2019-06-26 15:21:22 +0800

    Отредактировал контролер главной страницы, прижал футер, создал шаблон главной страницы

--- food_delivery/food_delivery/settings.py
+++ food_delivery/food_delivery/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'mainapp.apps.MainappConfig',
     'mainapp',
 ]
 
@@ -118,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = {
-    os.path.join(BASE_DIR, 'static'),
-}
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- food_delivery/mainapp/views.py
+++ food_delivery/mainapp/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 import random
+from .models import Product, ProductCategory, ProductCompany
 
 
 def get_random_restoran():
     restorans = ProductCompany.objects.all()
-
+    # return restorans[0]
     return random.sample(list(restorans), 1)[0]
 
 
 def get_hot_products():
     restoran = get_random_restoran()
-    products = Product.object.filter(restoran=restoran, is_active=True)
-    hot_products = random.sample(list(products), 3)[:3]
+    products = Product.objects.filter(company__name=restoran.name)
+    hot_products = random.sample(list(products), 3)[:2]
     return hot_products
 
 
@@ -20,6 +21,6 @@ def main(request):
     hot_products = get_hot_products()
 
     content = {'title': title,
-               'products': products,
+               'products': hot_products,
                }
     return render(request, 'mainapp/index.html', content)
