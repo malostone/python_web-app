c429f474988d51baa65f35f5de7ed78c9eaabdc5
Igor Maslovskiy <47256042+Ingvarrcoding@users.noreply.github.com>
2019-07-11 23:38:18 +0300

    Add files via upload
    Отредактировал создание заказа. Необходимо создать шаблоны заказа.

---
+++ food_delivery/ordersapp/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.conf import settings
+from mainapp.models import Products, ProductCategory, Restaurant
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    restauran = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
+    add_datetime = models.DateTimeField(verbose_name='время', auto_now_add=True)
+    price = models.DecimalField(verbose_name='Цена', max_digits=20, decimal_places=2, default=0)
+
+    class Meta:
+        ordering = ('-created', )
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return 'Заказ: {}'.format(self.id)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+    
+    def delete(self):
+        self.is_active = False
+        self.save()
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(verbose_name='Цена', max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(verbose_name='Количество', default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
