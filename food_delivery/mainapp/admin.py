6ce32cf0bff8538bff03413dd7c46e97d4478889
Pavel <pavel.dudkov@icloud.com>
2019-07-19 11:29:11 +0400

    1) Добавил настройки для админки 2) Добавил картинки для ресторанов

--- food_delivery/mainapp/admin.py
+++ food_delivery/mainapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import ProductCategory, Products
+from .models import ProductCategory, Products, Restaurant, RestaurantCategory
 
 admin.site.register(ProductCategory)
 admin.site.register(Products)
+admin.site.register(Restaurant)
+admin.site.register(RestaurantCategory)
